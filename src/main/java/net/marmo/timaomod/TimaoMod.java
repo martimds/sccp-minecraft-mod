6bcb0465fe593e6804effd988378c3006860925d
Martim <martimtimdias@gmail.com>
2023-03-10 22:18:26 -0300

    novo item

--- src/main/java/net/marmo/timaomod/TimaoMod.java
+++ src/main/java/net/marmo/timaomod/TimaoMod.java
@@ -2,6 +2,7 @@ package net.marmo.timaomod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.marmo.timaomod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TimaoMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/marmo/timaomod/item/ModItems.java
@@ -0,0 +1,33 @@
+package net.marmo.timaomod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.marmo.timaomod.TimaoMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item CORINTHIANS_DUST = registerItem("cornithians_dust",
+            new Item(new FabricItemSettings()));
+    private static Item registerItem(String name, Item item) {
+         return Registry.register(Registries.ITEM, new Identifier(TimaoMod.MOD_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup(){
+        addToItemGroup(ItemGroups.INGREDIENTS, CORINTHIANS_DUST);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item){
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+    public static void registerModItems(){
+        TimaoMod.LOGGER.info("Registrando itens do mod para " + TimaoMod.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+}
